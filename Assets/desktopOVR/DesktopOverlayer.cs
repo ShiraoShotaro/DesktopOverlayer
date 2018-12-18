a595c4df08b005c60dbc969a060d75b5cb30c432
wgprojects2013 <pastfantasy1640@gmail.com>
2018-12-18 15:22:50 +0900

    回転に対応

--- Assets/desktopOVR/DesktopOverlayer.cs
+++ Assets/desktopOVR/DesktopOverlayer.cs
@@ -51,6 +51,8 @@ public class DesktopOverlayer : MonoBehaviour
 	//上下反転フラグ
 	int textureYflip = 1;
 
+	HmdMatrix44_t transform_matrix;
+
 	//HMD視点位置変換行列
 	HmdMatrix34_t pose;
 
@@ -69,11 +71,80 @@ public class DesktopOverlayer : MonoBehaviour
 	[Range(-10f, 10f)]
 	public float posZ = -0.2f;
 
+	[Range(-360f, 360f)]
+	public float rotH = 0.0f;
+
+	[Range(-90f, 90f)]
+	public float rotV = 0.0f;
+
+	[Range(-180f, 180f)]
+	public float rotDisp = 0.0f;
+
+	public bool enable;
+	public void DisplayEnable()
+	{
+		//オーバーレイを表示する
+		if (overlay != null)
+		{
+			overlay.ShowOverlay(overlayHandle);
+		}
+	}
+	public void DisplayDisable()
+	{
+		//オーバーレイを非表示する
+		if (overlay != null)
+		{
+			overlay.HideOverlay(overlayHandle);
+		}
+	}
+
+	public bool zoom_enable;
+	public void ZoomEnalbe() { zoom_enable = true; }
+	public void ZoomDisable() { zoom_enable = false; }
 
 	public Slider ScaleSlider;
 	public Slider PosXSlider;
 	public Slider PosYSlider;
 	public Slider PosZSlider;
+	public Slider RotHSlider;
+	public Slider RotVSlider;
+	public Slider RotDispSlider;
+
+	HmdMatrix44_t mul(HmdMatrix44_t a, HmdMatrix44_t b)
+	{
+		HmdMatrix44_t ret;
+		ret.m0 = a.m0 * b.m0 + a.m1 * b.m4 + a.m2 * b.m8  + a.m3 * b.m12;
+		ret.m1 = a.m0 * b.m1 + a.m1 * b.m5 + a.m2 * b.m9  + a.m3 * b.m13;
+		ret.m2 = a.m0 * b.m2 + a.m1 * b.m6 + a.m2 * b.m10 + a.m3 * b.m14;
+		ret.m3 = a.m0 * b.m3 + a.m1 * b.m7 + a.m2 * b.m11 + a.m3 * b.m15;
+		ret.m4 = a.m4 * b.m0 + a.m5 * b.m4 + a.m6 * b.m8  + a.m7 * b.m12;
+		ret.m5 = a.m4 * b.m1 + a.m5 * b.m5 + a.m6 * b.m9  + a.m7 * b.m13;
+		ret.m6 = a.m4 * b.m2 + a.m5 * b.m6 + a.m6 * b.m10 + a.m7 * b.m14;
+		ret.m7 = a.m4 * b.m3 + a.m5 * b.m7 + a.m6 * b.m11 + a.m7 * b.m15;
+
+		ret.m8  = a.m8  * b.m0 + a.m9  * b.m4 + a.m10 * b.m8  + a.m11 * b.m12;
+		ret.m9  = a.m8  * b.m1 + a.m9  * b.m5 + a.m10 * b.m9  + a.m11 * b.m13;
+		ret.m10 = a.m8  * b.m2 + a.m9  * b.m6 + a.m10 * b.m10 + a.m11 * b.m14;
+		ret.m11 = a.m8  * b.m3 + a.m9  * b.m7 + a.m10 * b.m11 + a.m11 * b.m15;
+		ret.m12 = a.m12 * b.m0 + a.m13 * b.m4 + a.m14 * b.m8  + a.m15 * b.m12;
+		ret.m13 = a.m12 * b.m1 + a.m13 * b.m5 + a.m14 * b.m9  + a.m15 * b.m13;
+		ret.m14 = a.m12 * b.m2 + a.m13 * b.m6 + a.m14 * b.m10 + a.m15 * b.m14;
+		ret.m15 = a.m12 * b.m3 + a.m13 * b.m7 + a.m14 * b.m11 + a.m15 * b.m15;
+
+		return ret;
+	}
+
+	public void ResetDisplaySettings()
+	{
+		ScaleSlider.value = 1f;
+		PosXSlider.value = 0f;
+		PosYSlider.value = 0f;
+		PosZSlider.value = -2f;
+		RotHSlider.value = 0f;
+		RotVSlider.value = 0f;
+		RotDispSlider.value = 0f;
+		ChangedSlidersValue();
+	}
 
 	public void ChangedSlidersValue()
 	{
@@ -81,12 +152,71 @@ public class DesktopOverlayer : MonoBehaviour
 		posX = PosXSlider.value;
 		posY = PosYSlider.value;
 		posZ = PosZSlider.value;
+		rotH = RotHSlider.value;
+		rotV = RotVSlider.value;
+		rotDisp = RotDispSlider.value;
+
+		//行列の再計算
+
+		// 拡大行列
+		HmdMatrix44_t scale_mat;
+		scale_mat.m0 = scale; scale_mat.m1 = 0; scale_mat.m2 = 0; scale_mat.m3 = 0;
+		scale_mat.m4 = 0; scale_mat.m5 = scale * textureYflip; scale_mat.m6 = 0; scale_mat.m7 = 0;
+		scale_mat.m8 = 0; scale_mat.m9 = 0; scale_mat.m10 = scale; scale_mat.m11 = 0;
+		scale_mat.m12 = 0; scale_mat.m13 = 0; scale_mat.m14 = 0; scale_mat.m15 = 1;
+
+		// 平行移動行列
+		HmdMatrix44_t translate_mat;
+		translate_mat.m0 = 1; translate_mat.m1 = 0; translate_mat.m2 = 0; translate_mat.m3 = posX;
+		translate_mat.m4 = 0; translate_mat.m5 = 1; translate_mat.m6 = 0; translate_mat.m7 = posY;
+		translate_mat.m8 = 0; translate_mat.m9 = 0; translate_mat.m10 = 1; translate_mat.m11 = posZ;
+		translate_mat.m12 = 0; translate_mat.m13 = 0; translate_mat.m14 = 0; translate_mat.m15 = 1;
+
+		// 水平回転行列(Y)
+		HmdMatrix44_t rotH_mat;
+		float roth_rad = Mathf.Deg2Rad * rotH;
+		rotH_mat.m0 = Mathf.Cos(roth_rad); rotH_mat.m1 = 0; rotH_mat.m2 = Mathf.Sin(roth_rad); rotH_mat.m3 = 0;
+		rotH_mat.m4 = 0; rotH_mat.m5 = 1; rotH_mat.m6 = 0; rotH_mat.m7 = 0;
+		rotH_mat.m8 = -Mathf.Sin(roth_rad); rotH_mat.m9 = 0; rotH_mat.m10 = Mathf.Cos(roth_rad); rotH_mat.m11 = 0;
+		rotH_mat.m12 = 0; rotH_mat.m13 = 0; rotH_mat.m14 = 0; rotH_mat.m15 = 1;
+
+		// 垂直回転行列(X)
+		HmdMatrix44_t rotV_mat;
+		float rotv_rad = Mathf.Deg2Rad * rotV;
+		rotV_mat.m0 = 1; rotV_mat.m1 = 0; rotV_mat.m2 = 0; rotV_mat.m3 = 0;
+		rotV_mat.m4 = 0; rotV_mat.m5 = Mathf.Cos(rotv_rad); rotV_mat.m6 = -Mathf.Sin(rotv_rad); rotV_mat.m7 = 0;
+		rotV_mat.m8 = 0; rotV_mat.m9 = Mathf.Sin(rotv_rad); rotV_mat.m10 = Mathf.Cos(rotv_rad); rotV_mat.m11 = 0;
+		rotV_mat.m12 = 0; rotV_mat.m13 = 0; rotV_mat.m14 = 0; rotV_mat.m15 = 1;
+		
+		// ディスプレイ回転行列(Z)
+		HmdMatrix44_t rotDisp_mat;
+		float rotdisp_rad = Mathf.Deg2Rad * rotDisp;
+		rotDisp_mat.m0 = Mathf.Cos(rotdisp_rad); rotDisp_mat.m1 = -Mathf.Sin(rotdisp_rad); rotDisp_mat.m2 = 0; rotDisp_mat.m3 = 0;
+		rotDisp_mat.m4 = Mathf.Sin(rotdisp_rad); rotDisp_mat.m5 = Mathf.Cos(rotdisp_rad); rotDisp_mat.m6 = 0; rotDisp_mat.m7 = 0;
+		rotDisp_mat.m8 = 0; rotDisp_mat.m9 = 0; rotDisp_mat.m10 = 1; rotDisp_mat.m11 = 0;
+		rotDisp_mat.m12 = 0; rotDisp_mat.m13 = 0; rotDisp_mat.m14 = 0; rotDisp_mat.m15 = 1;
+
+		HmdMatrix44_t ret = translate_mat;
+		ret = mul(ret, scale_mat);
+		ret = mul(ret, rotH_mat);
+		ret = mul(ret, rotV_mat);
+		ret = mul(ret, rotDisp_mat);
+		//ret = mul(ret, rotH_mat);
+		//ret = mul(ret, rotDisp_mat);
+		//ret = mul(ret, translate_mat);
+		//ret = mul(ret, rot_mat);
+
+		pose.m0 = ret.m0; pose.m1 = ret.m1; pose.m2 = ret.m2; pose.m3 = ret.m3;
+		pose.m4 = ret.m4; pose.m5 = ret.m5; pose.m6 = ret.m6; pose.m7 = ret.m7;
+		pose.m8 = ret.m8; pose.m9 = ret.m9; pose.m10 = ret.m10; pose.m11 = ret.m11;
+
 	}
 
 	void Start()
 	{
 		var openVRError = EVRInitError.None;
 		var overlayError = EVROverlayError.None;
+
 
 		//OpenVRの初期化
 		openvr = OpenVR.Init(ref openVRError, EVRApplicationType.VRApplication_Overlay);
@@ -106,6 +236,7 @@ public class DesktopOverlayer : MonoBehaviour
 			ApplicationQuit();
 			return;
 		}
+
 
 		//オーバーレイの大きさ設定(幅のみ。高さはテクスチャの比から自動計算される)
 		var width = 2.0f;
@@ -130,6 +261,13 @@ public class DesktopOverlayer : MonoBehaviour
 			//上下反転する
 			textureYflip = -1;
 		}
+
+		//変換行列の初期設定
+		ChangedSlidersValue();
+
+		//オーバーレイを表示する
+		overlay.ShowOverlay(overlayHandle);
+
 		Debug.Log(Tag + "初期化完了しました");
 	}
 
@@ -141,24 +279,12 @@ public class DesktopOverlayer : MonoBehaviour
 			return;
 		}
 
-		//オーバーレイを表示する
-		overlay.ShowOverlay(overlayHandle);
-
-		//オーバーレイを非表示にする
-		//overlay.HideOverlay(overlayHandle);
-
 		//オーバーレイが表示されている時
 		if (overlay.IsOverlayVisible(overlayHandle))
 		{
 			//HMD視点位置変換行列に書き込む。
 			//ここでは回転なし、平行移動ありのHUD的な状態にしている。
-			var wx = posX;
-			var wy = posY;
-			var wz = posZ;
-
-			pose.m0 = scale; pose.m1 = 0; pose.m2 = 0; pose.m3 = wx;
-			pose.m4 = 0; pose.m5 = scale * textureYflip; pose.m6 = 0; pose.m7 = wy;
-			pose.m8 = 0; pose.m9 = 0; pose.m10 = scale; pose.m11 = wz;
+			
 
 			//回転行列を元に、HMDからの相対的な位置にオーバーレイを表示する。
 			//代わりにSetOverlayTransformAbsoluteを使用すると、ルーム空間に固定することができる
